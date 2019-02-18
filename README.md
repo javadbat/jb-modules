391778b4ab62517971edbd503a6a382459777fd4
javad <javadbat@gmail.com>
2019-02-18 16:58:49 +0330

    add jb modules o npm

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-jb-modules
+
+#jb-modules
+
+set of js tools to help you write better code
 
 its not ready for public use now but it will
---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports.PersianCulture = require('./Culture/dist/PersianCulture.min.js');
+module.exports.DateTimeConverter = require('./DateTimeModule/dist/DateTimeConverter.min.js');
+module.exports.ExceptionHandler = require('./ExceptionHandler/dist/ExceptionHandler.min.js');
+module.exports.FetchHandler = require('./FetchHandler/dist/FetchHandler.min.js');
+module.exports.RequestData = require('./RequestModule/dist/RequestData.min.js');
+module.exports.UploadFileRequest = require('./RequestModule/dist/UploadFileRequest.min.js');
